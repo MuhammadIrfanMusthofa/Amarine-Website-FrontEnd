c166e70abc66d18c3e3ea1efc2be119f729b9c33
Michael Fernando King Luise <114063387+MichaelFernandoKingLuise@users.noreply.github.com>
2024-11-09 20:24:20 +0700

    Delete lupa_pass directory

--- lupa_pass/script.js
+++
@@ -1,8 +0,0 @@
-const otpInputs = document.querySelectorAll(".otp-digit");
-otpInputs.forEach((input, index) => {
-  input.addEventListener("input", () => {
-    if (input.value.length === 1 && index < otpInputs.length - 1) {
-      otpInputs[index + 1].focus();
-    }
-  });
-});
